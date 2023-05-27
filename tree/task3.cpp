9daff89289ad54dd2af90ae09059382f0f5ac72f
YuraGolyshev <yura.golyshev2021@bk.ru>
2023-05-27 14:27:59 +0400

    Реализовал пример

--- tree/task3.cpp
+++ tree/task3.cpp
@@ -1,169 +1,229 @@
 #include <iostream>
-
+#include <fstream>
+#include <cmath>
+#include <queue>
 using namespace std;
 
-struct tree{
+ifstream in ("input.txt");
+
+struct tree {
 int inf;
-tree *left;
 tree *right;
-tree *parent;
+tree * left ;
 };
 
 tree *node(int x){
 tree *n = new tree;
 n->inf = x;
-n->parent = NULL;
-n->right = NULL;
-n->left = NULL;
+n->left = n->right = NULL;
+return n;
 }
 
-void insert(tree *&tr, int x){//вставка
-    tree *n = node(x);
-    if (! tr ) tr = n; //если дерево пустое - корень
-    else {
-        tree *y = tr;
-        while(y){ //ищем куда вставлять
-            if (n->inf > y->inf) //правая ветка
-            if (y->right)
-            y = y->right;
-            else{
-                n->parent = y; //узел становится правым ребенком
-                y->right = n;
-                break;
-            }
-            else if (n->inf < y->inf)//левая ветка
-                if (y->left)
-                    y = y->left;
-                else{
-                    n->parent = y;//узел становится левым ребенком
-                    y->left = n;
-                    break;
-                }
-        }
-    }
+
+void create(tree *&tr, int n){
+int x;
+if (n > 0){
+in >> x;
+tr = node(x);
+int nl = n/2;
+int nr = n - nl - 1;
+create ( tr->left, nl);
+create ( tr->right,nr);
+}
 }
 
-void inorder(tree *tr){//симметричный обход
-if(tr){
-inorder ( tr->left);
+void preorder(tree *tr){
+
+if (tr) {
 cout << tr->inf << " ";
-inorder ( tr->right);
+preorder( tr->left);
+preorder( tr->right);
 }
 }
 
-tree *find(tree *tr, int x){//поиск
-    if (! tr || x == tr->inf)//нашли или дошли до конца ветки
-        return tr;
-    if (x < tr->inf)
-        return find(tr->left, x);//ищем по левой ветке
-    else
-        return find(tr->right, x);//ищем по правой ветке
+int lefth(tree *tr){
+int k = 0;
+tree *x = tr;
+while (x){
+k++;
+x = x->left;
+}
+return k - 1;
 }
 
-tree *Min(tree *tr){//поиск min
-    if (! tr->left) return tr;//нет левого ребенка
-    else return Min(tr->left);//идем по левой ветке до конца
+int righth(tree *tr){
+int k = 0;
+tree *x = tr;
+while (x){
+k++;
+x = x->right;
 }
-
-tree *Max(tree *tr){//поиск max
-if (! tr->right) return tr;//нет правого ребенка
-else return Max(tr->right);//идем по правой ветке до конца
-}
-
-tree *Next(tree*tr, int x){//поиск следующего
-    tree* n = find( tr , x);
-    if (n->right)//если есть правый ребенок
-        return Min(n->right);//min по правой ветке
-    tree *y = n->parent; //родитель
-    while (y && n == y->right){//пока не дошли до корня или узел - правый ребенок
-        n = y;//идем вверх по дереву
-        y = y->parent;
-    }
-    return y;//возвращаем родителя
-}
-
-tree *Prev(tree *tr, int x){//поиск предыдущего
-tree *n = find(tr , x);
-if (n->left)//если есть левый ребенок
-return Max(n->left);//max по левой ветке
-tree *y = n->parent;//родитель
-while(y && n == y->left){//пока не дошли до корня или узел - левый ребенок
-n = y;//идем вверх по дереву
-y = y->parent;
-}
-return y;//возвращаем родителя
+return k - 1;
 }
 
 
-void Delete(tree *&tr, tree *v){//удаление узла
-    tree *p = v->parent;
-    if (!p) tr = NULL; //дерево содержит один узел
-    else if (!v->left && !v->right){//если нет детей
-        if (p->left == v) //указатель у родителя меняем на NULL
-            p->left = NULL;
-        if (p->right == v)
-            p->right = NULL;
-        delete v;
-    }
-    else if (!v->left || !v->right){//если только один ребенок
-        if (!p) { //если удаляем корень, у которого 1 ребенок
-            if (!v->left){ //если есть правый ребенок
-                tr = v->right; //он становится корнем
-                v->parent = NULL;
-            }
-            else { //аналогично для левого
-                tr = v->left;
-                v->parent = NULL;
-            }
-        }
-        else {
-            if (!v->left){//если есть правый ребенок
-                if (p->left == v) //если удаляемый узел явл. левым ребенком
-                    p->left = v->right; //ребенок удаляемого узла становится левым ребенком своего "деда"
-                else
-                    p->right = v->right; ////ребенок удаляемого узла становится правым ребенком своего "деда"
-                v->right->parent = p; //родителем ребенка становится его "дед"
-            }
-            else{//аналогично для левого ребенка
-                if (p->left == v)
-                    p->left = v->left;
-                else
-                    p->right = v->left;
-                v->left->parent = p;
-            }
-            delete v;
-        }
-    }
-    else{//есть оба ребенка
-        tree *succ = Next(tr, v->inf);//следующий за удаляемым узлом
-        v->inf = succ->inf; //присваиваем значение
-        if (succ->parent->left ==succ){//если succ левый ребенок
-            succ->parent->left = succ->right; //его правый ребенок становится левым ребенком своего "деда"
-            if (succ->right) //если этот ребенок существует
-                succ->right->parent = succ->parent; //его родителем становится "дед"
-        }
-        else {//аналогично если succ - правsq ребенок
-            succ->parent->right = succ->right;
-            if (succ->right)
-                succ->right->parent = succ->parent;
-        }
-        delete succ;
-    }
+void add(tree *&tr, int x){
+tree *n = node(x);
+tree *y = tr;
+if (lefth ( tr ) == righth(tr)){
+do{
+y = y->left;
+}
+while (y->left);
+if (!y->left) y->left = n;
+else y->right = n;
+}
+else{
+do{
+y = y->right;
+}
+while (y->right);
+if (!y->left) y->left = n;
+else y->right = n;
+}
+
+}
+
+void find(tree *tr, int x, tree *&res){
+if (tr){
+if (tr->inf == x){
+res = tr;
+}
+else {
+find ( tr->left, x, res);
+find ( tr->right, x, res);
+}
+}
+}
+
+void del_n(tree *tr, int val){
+tree *y;
+find ( tr , val, y);
+if (y){
+if(lefth( tr ) == 0) tr = NULL;
+else if(lefth(tr) != righth(tr )){
+tree *x = tr->left;
+do{
+x = x->left;
+}
+while(x->left->left);
+if(x->right){
+if(x->right->inf == val){
+x->right = NULL;
+}
+else{
+y->inf = x->right->inf;
+x->right = NULL;
+}
+delete x->right;
+}
+else{
+if(x->left->inf == val){
+x->left = NULL;
+}
+else{
+y->inf = x->left->inf;
+x->left = NULL;
+}
+delete x->left;
+}
+}
+else{
+tree *x = tr->right;
+do{
+x = x->right;
+}
+while(x->right->right);
+if(x->right){
+if(x->right->inf == val){
+x->right = NULL;
+}
+else{
+y->inf = x->right->inf;
+x->right = NULL;
+
+}
+delete x->right;
+}
+else{
+if(x->left->inf == val){
+x->left = NULL;
+}
+else{
+y->inf = x->left->inf;
+x->left = NULL;
+}
+delete x->left;
+}
+}
+}
+
+}
+
+void print(tree *tr, int k){
+if (! tr ) cout << "Empty tree\n";
+else{
+queue<tree*> cur, next;
+tree *r = tr;
+cur.push(r);
+int j = 0;
+while (cur.size()){
+if (j == 0) {
+for (int i = 0; i < (int)pow(2.0, k) - 1; i++)
+cout << ' ';
+}
+tree *buf = cur.front () ;
+cur.pop();
+j++;
+if (buf){
+cout << buf->inf;
+next.push(buf->left);
+next.push(buf->right);
+for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+cout << ' ';
+}
+if (!buf){
+for (int i = 0; i < (int)pow(2.0, k + 1) - 1; i++)
+cout << ' ';
+cout << ' ';
+}
+if(cur.empty()){
+cout << endl;
+swap(cur, next);
+j = 0;
+k--;
+}
+}
+}
 }
 
 
-
-
-int main()
-{
-int n, x;
-cout << "n="; cin >> n;
+int main(){
 tree *tr = NULL;
-cout << "BBedite elemeti" << endl;
-for(int i = 0; i < n; i++){
+int n, x;
+in >> n;
+create ( tr , n);
+int k = int (log((float)n)/log((float)2.0));
+print ( tr , k);
+preorder( tr );
+cout << endl;
+cout << lefth(tr) << " " << righth(tr);
+cout << endl;
+cout << "x=";
 cin >> x;
-insert(tr, x);
-}
-
+add(tr,x);
+n++;
+k = int (log((float)n)/log((float)2.0));
+print ( tr , k);
+preorder( tr );
+cout << endl;
+cout << " del node: ";
+cin >> x;
+del_n(tr , x);
+n--;
+k = int (log((float)n)/log((float)2.0));
+print ( tr , k);
+preorder( tr );
+cout << endl;
 return 0;
 }
