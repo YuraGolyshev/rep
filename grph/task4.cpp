c849aac65399acb6e2e88f03413d43be1d42a1a1
YuraGolyshev <yura.golyshev2021@bk.ru>
2023-05-29 19:42:54 +0400

    Начало работы над 4

---
+++ grph/task4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>  
+#include <map>
+#include <set>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+ifstream in ("input.txt");
+
+int main()
+{
+vector<vector<int>> Gr;			//Cписок смежности
+	int n, r;
+
+	cout << "n = "; // кол-во вершин
+	in >> n;
+	cout << n << endl;
+
+	cout << "r = "; // кол-во ребер
+	in >> r;
+	cout << r << endl;
+
+	Gr.resize(n);					
+
+	while (in.peek() != EOF) {		
+		int x, y;
+		in >> x >> y;
+		if (x > n || y > n) { // ор
+		}
+		else {
+			Gr[x].push_back(y);				
+		}
+	}
+
+	for (int i = 0; i < n; i++) {	
+		sort(Gr[i].begin(), Gr[i].end());
+		Gr[i].erase(unique(Gr[i].begin(), Gr[i].end()), Gr[i].end());
+	}
+
+	cout << "\nsp smewn:\n"; // вывод списка сможности
+	for (int i = 0; i < n; i++) {	// вывод содержимого списка смежности
+		cout << "x = " << i << ": ";
+		for (auto it = Gr[i].begin(); it != Gr[i].end(); it++) {
+			cout << *it << ' ';
+		}
+		cout << endl;
+	}
+    
+    
+
+    // for (int i = 0; i < n; i++) {
+	// 	int c = 0;
+
+	// 	for (int j = 0; j < n; j++) {
+	// 		c += count(Gr[i].begin(), Gr[i].end(), j);	//В счётчик count прибавляем, можно ли попасть из данной вершины в вершину j, и так делаем с каждой вершиной
+	// 	}
+
+	// 	cout << "ish_" << i << " = " << c << endl;
+	// }
+}
